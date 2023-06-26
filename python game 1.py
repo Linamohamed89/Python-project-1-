b405c3dc5968cac859ffe1edc7d0fe33a2933039
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-26 22:11:39 +0200

    add  new file

---
+++ python game 1.py
@@ -0,0 +1,8 @@
+'''
+     -start: welcome message , games options
+     -enter game number
+     -option : option to exit
+     -start play game
+     -finish game : play again
+     -yes : play again - no : exit 
+'''
