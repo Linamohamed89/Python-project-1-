b5f0c89abee79e62f778e1cb7db4d096ab9e542f
Linamohamed89 <linaaloqaily1989@gmail.com>
2023-06-26 22:21:17 +0200

    create python game 1

--- python game 1.py
+++ python game 1.py
@@ -6,3 +6,16 @@
      -finish game : play again
      -yes : play again - no : exit 
 '''
+
+
+#game :names seperated , length ---> names < length
+
+def fillter_by_length():
+    names=input('Enter Names:')
+    print(names)
+
+
+fillter_by_length()
+    
+
+
